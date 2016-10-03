7fb33f5cab9ad310af974476e46d52a7b1b96266
Ben Makusha <btmakusha@gmail.com>
2016-10-03 01:57:08 -0400

    Setting up facebook authentication part 1

--- social-showcase/SignInVC.swift
+++ social-showcase/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -15,6 +18,33 @@ class SignInVC: UIViewController {
         
     }
 
+    @IBAction func facebookBtnTapped(_ sender: AnyObject) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("BEN : Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("BEN: User cancelled Facebook authentication")
+            } else {
+                print("BEN: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("BEN: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("BEN: Successfully authenticated with Firebase")
+            }
+        })
+    }
 
 }
 
