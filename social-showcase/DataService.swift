9ee908bfc86b88a8760ece1e54d9179275909966
Ben Makusha <btmakusha@gmail.com>
2016-10-14 04:42:40 -0400

    Established Firebase database relationships when it comes to likes. - Established likes to increment and decrement

--- social-showcase/DataService.swift
+++ social-showcase/DataService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Firebase
+import SwiftKeychainWrapper
 
 let DB_BASE = FIRDatabase.database().reference()            //This will contain the URI for our database
 let STORAGE_BASE = FIRStorage.storage().reference()         //This will contain the URI for our storage portion of firebase
@@ -37,6 +38,12 @@ class DataService {
     
     var REF_POST_IMAGES: FIRStorageReference {
         return _REF_POST_IMAGES
+    }
+    
+    var REF_USER_CURRENT: FIRDatabaseReference {
+        let uid = KeychainWrapper.standard.string(forKey: KEY_UID)
+        let user = REF_USERS.child(uid!)
+        return user
     }
     
     func createFirebaseDBUser(uid: String, userData: Dictionary<String, String>) {
